51c15cb5aa8796331cbf571a54da9b2ec6750b43
Jamie Element <sc12jcce@leeds.ac.uk>
2016-03-16 14:45:02 +0000

    Added files
    Added all the files

---
+++ File Finder/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace File_Finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnDeleteSelected = new System.Windows.Forms.Button();
+            this.chkFiles = new System.Windows.Forms.CheckedListBox();
+            this.lblDir = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.prgProgress = new System.Windows.Forms.ProgressBar();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnDeleteAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(321, 13);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(57, 13);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(258, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnDeleteSelected
+            // 
+            this.btnDeleteSelected.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnDeleteSelected.AutoSize = true;
+            this.btnDeleteSelected.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnDeleteSelected.Location = new System.Drawing.Point(267, 265);
+            this.btnDeleteSelected.Name = "btnDeleteSelected";
+            this.btnDeleteSelected.Size = new System.Drawing.Size(93, 23);
+            this.btnDeleteSelected.TabIndex = 4;
+            this.btnDeleteSelected.Text = "Delete Selected";
+            this.btnDeleteSelected.UseVisualStyleBackColor = true;
+            this.btnDeleteSelected.Click += new System.EventHandler(this.btnDeleteSelected_Click);
+            // 
+            // chkFiles
+            // 
+            this.chkFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkFiles.CheckOnClick = true;
+            this.chkFiles.FormattingEnabled = true;
+            this.chkFiles.Location = new System.Drawing.Point(12, 76);
+            this.chkFiles.Name = "chkFiles";
+            this.chkFiles.Size = new System.Drawing.Size(660, 184);
+            this.chkFiles.TabIndex = 5;
+            // 
+            // lblDir
+            // 
+            this.lblDir.AutoSize = true;
+            this.lblDir.Location = new System.Drawing.Point(12, 15);
+            this.lblDir.Name = "lblDir";
+            this.lblDir.Size = new System.Drawing.Size(39, 13);
+            this.lblDir.TabIndex = 6;
+            this.lblDir.Text = "Folder:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnClose.AutoSize = true;
+            this.btnClose.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnClose.Location = new System.Drawing.Point(426, 265);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(43, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnRefresh.AutoSize = true;
+            this.btnRefresh.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnRefresh.Location = new System.Drawing.Point(366, 265);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(54, 23);
+            this.btnRefresh.TabIndex = 8;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // prgProgress
+            // 
+            this.prgProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.prgProgress.Location = new System.Drawing.Point(12, 50);
+            this.prgProgress.Name = "prgProgress";
+            this.prgProgress.Size = new System.Drawing.Size(660, 23);
+            this.prgProgress.TabIndex = 9;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(446, 13);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 13);
+            this.lblTime.TabIndex = 10;
+            // 
+            // btnDeleteAll
+            // 
+            this.btnDeleteAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.btnDeleteAll.AutoSize = true;
+            this.btnDeleteAll.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnDeleteAll.Location = new System.Drawing.Point(199, 265);
+            this.btnDeleteAll.Name = "btnDeleteAll";
+            this.btnDeleteAll.Size = new System.Drawing.Size(62, 23);
+            this.btnDeleteAll.TabIndex = 11;
+            this.btnDeleteAll.Text = "Delete All";
+            this.btnDeleteAll.UseVisualStyleBackColor = true;
+            this.btnDeleteAll.Click += new System.EventHandler(this.btnDeleteAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 297);
+            this.Controls.Add(this.btnDeleteAll);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.prgProgress);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblDir);
+            this.Controls.Add(this.chkFiles);
+            this.Controls.Add(this.btnDeleteSelected);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnBrowse);
+            this.MinimumSize = new System.Drawing.Size(430, 335);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnDeleteSelected;
+        private System.Windows.Forms.CheckedListBox chkFiles;
+        private System.Windows.Forms.Label lblDir;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ProgressBar prgProgress;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnDeleteAll;
+    }
+}
+
---
+++ File Finder/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace File_Finder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+
+            if (folderBrowserDialog1.SelectedPath != "")
+            {
+                this.txtPath.Text = folderBrowserDialog1.SelectedPath;
+                findFiles();
+            }
+        }       
+        
+        private void btnDeleteAll_Click(object sender, EventArgs e)
+        {
+            if (chkFiles.Items.Count > 0)
+            {
+                if (MessageBox.Show("Are you sure you wish to delete ALL items?", "Confirm delete", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    string results = "";
+
+                    for (int i = 0; i < chkFiles.Items.Count; i++)
+                    {
+                        new FileInfo(chkFiles.Items[i].ToString()).Delete();
+                        results += chkFiles.Items[i].ToString() + " - Deleted. \r\n";
+                    }
+                    MessageBox.Show(results);
+                    ResetForm();
+                }
+            }
+        }
+
+        private void btnDeleteSelected_Click(object sender, EventArgs e)
+        {
+            int chkNum = 0;
+            for (int j = 0; j < chkFiles.Items.Count; j++)
+            {
+                if (chkFiles.GetItemChecked(j) == true) { chkNum++; }
+            }
+
+            if(chkNum == 0)
+            {
+                MessageBox.Show("Please check at least one box");
+                return;
+            }
+
+            if (chkFiles.Items.Count > 0)
+            {
+                if (MessageBox.Show("Are you sure you wish to delete the selected items?", "Confirm delete", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    string results = "";
+
+                    for (int i = 0; i < chkFiles.Items.Count; i++)
+                    {
+                        if (chkFiles.GetItemChecked(i) == true)
+                        {
+                            new FileInfo(chkFiles.Items[i].ToString()).Delete();
+                            results += chkFiles.Items[i].ToString() + " - Deleted. \r\n";
+                        }
+                    }
+                    MessageBox.Show(results);
+                    ResetForm();
+                }
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+            findFiles();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+
+        private void ResetForm()
+        {
+            prgProgress.Value = 0;
+            chkFiles.Items.Clear();
+            lblTime.Text = "";
+        }
+        
+        private List<String> DirSearch(string sDir)
+        {
+            List<String> files = new List<String>();
+            string txtFiles = "";
+            try
+            {
+                foreach (string file in Directory.GetFiles(sDir))
+                {
+                    if (file.StartsWith(sDir + "\\" + "._"))
+                    {
+                        files.Add(file);
+                    }
+                }
+                foreach (string dir in Directory.GetDirectories(sDir))
+                {
+                    files.AddRange(DirSearch(dir));
+                }
+            }
+            catch (System.Exception exc)
+            {
+                txtFiles += "\r\n" + exc.Message;
+            }
+            return files;
+        } 
+        
+        private void findFiles()
+        {
+            Stopwatch stp = new Stopwatch();
+            stp.Start();
+            DirectoryInfo dir = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+
+            string TopDIR = dir.ToString();
+            List<String> files = DirSearch(TopDIR);
+            prgProgress.Maximum = files.Count + 50;
+            prgProgress.Value = 50;
+
+            foreach (string file in files)
+            {
+                this.chkFiles.Items.Add(new CheckBox().Text = file);
+                if (prgProgress.Value != prgProgress.Maximum) { prgProgress.Value += 1; lblTime.Text = "Time Taken: " + stp.Elapsed; }
+            }
+
+            stp.Stop();
+            TimeSpan ts = stp.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            lblTime.Text = "Time Taken: " + elapsedTime + "   Files: " + files.Count.ToString();
+        }
+    }
+}
